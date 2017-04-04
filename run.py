29694cccd9446b8ea20062a0d80736e50082f1c8
vgorin <vasilygorin@gmail.com>
2017-04-04 20:37:34 +0300

    Make subparsers required

--- run.py
+++ run.py
@@ -15,12 +15,15 @@ def run_rdp_connect(args):
 
 
 def cli():
-    parser = argparse.ArgumentParser(prog="Programm summary",
+    # TODO descriptions
+    parser = argparse.ArgumentParser(prog="VM manager",
                                      description="Description")
 
-    subparsers = parser.add_subparsers()
+    subparsers = parser.add_subparsers(dest='command')
+    subparsers.required = True
 
-    parser_rdp = subparsers.add_parser('rdp')
+    parser_rdp = subparsers.add_parser('rdp',
+                                       description='Open RDP to specified VM')
     parser_rdp.add_argument('-n', '--vm-name', type=str, help='vm name',
                             required=True)
     parser_rdp.add_argument('-s', '--suffix', type=str,
@@ -28,13 +31,13 @@ def cli():
                             help='Default: {}'.format(config.DEFAULT_SUFFIX))
     parser_rdp.set_defaults(func=run_rdp_connect)
 
-    parser_show = subparsers.add_parser('list')
+    parser_show = subparsers.add_parser('list',
+                                        description='List of VMs')
     parser_show.add_argument('-n', '--vm-name', type=str,
                              help='part of vm name')
     parser_show.add_argument('-v', '--verbose', action='store_true',
                              help='verbose output')
     parser_show.set_defaults(func=run_show_list)
-    # TODO print help if call run.py without arguments
 
     return parser.parse_args()
 
